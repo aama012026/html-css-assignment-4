0f8af51c30942c4b7bc7df3b9471922677f3b558
aama012026 <aama012026@gmail.com>
2026-03-19 07:01:16 +0100

    Added data.js

---
+++ assets/data/reviews.js
@@ -0,0 +1,63 @@
+export const reviews = [[
+		{
+			name: 'Roxanne',
+			img: 'acrylic/kid-mess.jpg',
+			text: 'Went with my kids. Had a great time learning to paint. I hope the paint washes off the clothes.'
+		},
+		{
+			name: 'Thea',
+			img: 'acrylic/teal-mess.jpg',
+			text: 'Wow! So fun! Struggled to get the hang of it, but felt like a real pro when brushing on a big canvas.'
+		}
+	],
+	[
+		{
+			name: 'Flower-lady',
+			img: 'watercolor/blue-magenta-violet-flowers.jpg',
+			text: 'Behold! My mighty metal composition of the coolest flowers IN THE ENTIRE WOOOORLD! mUAHAHA I will make evil descend upon the world when the witches reign returns.'
+		},
+		{
+			name: 'performative-male98',
+			img: 'watercolor/blue-orange-comp',
+			text: 'Just casually enjoying this advanced watercoloring class. Nothing mutch. Just happened to pass by the place and thought I try anything once. Maybe you wanna try something out with me? ;)'
+		},
+		{
+			name: 'PaintWithJenny',
+			img: 'watercolor/blue-woman.jpg',
+			text: 'Incredible place with such talented staff. I always recommend this place to newcomers when viewers ask me. Such a bummer this boy of a man was trying to get it on, constantly looking at my work and copying techniques. I had to tell him to let me work in peace multiple times.'
+		},
+		{
+			name: 'Per Harald',
+			img: 'watercolor/dark-violet-flower.jpg',
+			text: 'Had a nice workshop with my colleagues in watercolor painting, and we should be really proud of ourselves. Not many persist and complete what we have. Gave ourselves a pat on the back and took a well deserved break afterwards.'
+		},
+		{
+			name: 'David AfterBurrow',
+			img: 'watercolor/jungle.jpg',
+			text: 'And here we have a watercolorpainter in its natural habitat. Painters are curious little creatures. They get their calsium from licking paint pucks of different pigments.'
+		}
+
+	],
+	[
+		{
+			name: 'Pikmins mom',
+			img: 'mosaic/colored-gem-rocks.jpg',
+			text: "Look at what my little darlings made! I'm so proud of them. Lovely staff."
+		},
+		{
+			name: 'Larred Jeto',
+			img: 'mosaic/fishes.jpg',
+			text: 'The facilities were well equipped and the staff was friendly. Very informative and newbe friendly workshop. Not that I needed that of course.'
+		},
+		{
+			name: 'Rob the Builder',
+			img: 'mosaic/mini-tiling.jpg',
+			text: "The workshop was ok I guess, but I didn't really know what to make. The instructor wasn't that helpful with just build what you feel like. Anyways, made some miniature pavement."
+		},
+		{
+			name: 'Emma2k04-followonInstaforMore',
+			img: 'mosaic/pebble-comp.jpg',
+			text: 'Lovely space. They set up the perfect atmosphere. Although I suggest you bring your own rocks. The ones they have here are not as instagrammable.'
+		}
+	]
+]
